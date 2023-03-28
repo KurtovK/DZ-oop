9c7bb62f25b9e3c2c06b23a7a640ac9402139c16
KKD <kossstyan87@mail.ru>
2023-03-28 15:40:04 +0300

    Добавил функцию метод класса

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ from typing import Dict
 #Реализуйте доступ к отдельным полям класса через методы класса (геттеры
 #и сеттеры).
 
+
+class Human:
+    name: Dict[str: str]
+    data_birthday: Dict[str, str]
+    phone: str
+    country:str
+    city: str
+    address: str
+
+    def __init__(self,name: Dict[str,str],data_birthday: Dict[str, str], phone: str,
+                country:str, city: str, address: str):
+        self.name = name
+        self.data_birthday = data_birthday
+        self.phone = phone
+        self.city = city
+        self.address = address
+    def __str__(self):
+        return  f"ФИО: {list(self.name.values())}\n" \
+                f"Дата рождения: {list(self.data_birthday.values())}\n"\
+                f"Номер телефона: {self.phone}\n" \
+                f"Страна: {self.city}\n"\
+                f"Адресс: {self.address}\n"
+
 def execute_application():
-    class Human:
-        name: Dict[str: str]
-        data_birthday: Dict[str, str]
-        phone: str
-        country:str
-        city: str
-        address: str
-
-        def __init__(self,name: Dict[str,str],data_birthday: Dict[str, str], phone: str,
-                     country:str, city: str, address: str):
-            self.name = name
-            self.data_birthday = data_birthday
-            self.phone = phone
-            self.city = city
-            self.address = address
-
-
+    pass
 
 
 if __name__=="__main__":
